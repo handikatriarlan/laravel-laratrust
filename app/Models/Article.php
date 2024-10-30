578ccfccc0687e30b0da9ae7decda076db23b9f1
Arlann <handikaarlan@gmail.com>
2024-10-30 13:42:20 +0700

    first commit

---
+++ app/Models/Article.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Article extends Model
+{
+    /** @use HasFactory<\Database\Factories\ArticleFactory> */
+    use HasFactory;
+
+    protected $guarder = [];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
